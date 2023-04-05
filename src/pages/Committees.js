83e3a92a055d313b58131154683e357cd35eccdc
Lorenzo Carnevale <lcarnevale@unime.it>
2023-04-05 16:53:33 +0200

    add co-chairing for general positions

--- src/pages/Committees.js
+++ src/pages/Committees.js
@@ -12,7 +12,7 @@ const Committees = () => {
                     <tbody>
                         <tr>
                             <th>
-                                <h3>General Chair(s)</h3>
+                                <h3>General Co-Chair(s)</h3>
                             </th>
                         </tr>
                         <tr>
