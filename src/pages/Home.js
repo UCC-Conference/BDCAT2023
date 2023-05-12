d8fd2eceabd15cf21067b348c268c68e7c239e7e
Lorenzo Carnevale <lorenzocarnevale@gmail.com>
2023-05-12 11:50:34 +0200

    uniform the bdcat introduction with the cfp

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -38,13 +38,13 @@ const Home = () => {
                     <h1>About BDCAT</h1>
                 </center>
                 <p>
-                The IEEE/ACM International Conference on Big Data Computing, Applications and Technologies (BDCAT) is an annual conference series aiming to provide a platform for researchers from both academia and industry to present new discoveries in the broad area of big data computing and applications.
+                    The IEEE/ACM International Conference on Big Data Computing, Applications, and Technologies (BDCAT) is a premier annual conference series aiming to provide a platform for researchers from both academia and industry to present new discoveries in the broad area of big data computing and applications.
                 </p>
                 <p>
-                This will be the 10th BDCAT in a successful conference series of community-driven events. Previous events were held in London, UK (BDCAT 2014), Limassol, Cyprus (BDCat 2015), Shanghai, China (BDCAT 2016), Austin, Texas, USA (BDCAT 2017), Zurich, Switzerland (BDCAT 2018), Auckland, New Zealand (BDCAT 2019), Leicester, UK (BDCAT 2020) and Leicester, UK (BDCAT 2021) and Vancouver, USA (BDCAT 2022).
+                    Previous events were held in London, UK (BDCAT 2014), Limassol, Cyprus (BDCAT 2015), Shanghai, China (BDCAT 2016), Austin, USA (BDCAT 2017), Zurich, Switzerland (BDCAT 2018), Auckland, New Zealand (BDCAT 2019), Leicester, UK (BDCAT 2020), Leicester, UK (BDCAT 2021) and Vancouver, USA (BDCAT 2022). 
                 </p>
                 <p>
-                BDCAT2023 will be held in conjunction with the 16th IEEE/ACM International Conference on Utility and Cloud Computing.
+                    BDCAT 2023 will be held in conjunction with the 16th IEEE/ACM International Conference on Utility and Cloud Computing (UCC 2023) in Taormina, Messina, Italy.
                 </p>
             </Content>
             <Content className="center">
