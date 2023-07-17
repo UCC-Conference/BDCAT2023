bfd103828526e9c5fd3624435ff3b35e2b4a0067
Mario Colosi <colosimario96@gmail.com>
2023-07-17 20:00:23 +0200

    add: technical program committee added

---
+++ src/pages/TechnicalProgramCommittee.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Content from "../components/elements/Content";
+import PageWrapper from "../components/elements/PageWrapper";
+
+const SteeringCommittee = () => {
+    return (
+        <PageWrapper>
+            <Content>
+                <h1>Technical Program Committee</h1>
+                <hr />
+                <table>
+                    <tbody>
+                    <tr><td>Adel Taweel, BZU/KCL</td></tr>
+                    <tr><td>Alex Sim, Lawrence Berkeley National Laboratory, USA</td></tr>
+                    <tr><td>Ali Mirarab, ISCA</td></tr>
+                    <tr><td>Carlos Reaño, Universitat de València, Spain</td></tr>
+                    <tr><td>Cheol-Ho Hong, Chung-Ang University, South Corea</td></tr>
+                    <tr><td>Da Zhang, Samsung Semiconduct Inc.</td></tr>
+                    <tr><td>Dalibor Klusacek, CESNET, Czech Republic</td></tr>
+                    <tr><td>Daniel Waddington, IBM Research</td></tr>
+                    <tr><td>David Chiu, University of Puget Sound, USA</td></tr>
+                    <tr><td>Dongfang Zhao, University of Nevada</td></tr>
+                    <tr><td>Fabiana Zollo, Ca' Foscari University, Italy</td></tr>
+                    <tr><td>In Kee Kim, University of Georgia, Georgia</td></tr>
+                    <tr><td>Jason Sawin, University of St. Thomas, USA</td></tr>
+                    <tr><td>Jian Tao, Texas A&M University, USA</td></tr>
+                    <tr><td>Jianwu Wang, University of Maryland, USA</td></tr>
+                    <tr><td>Jiayin Wang, Montclair State University, USA</td></tr>
+                    <tr><td>Junqi Yin, Oak Ridge National Laboratory, USA</td></tr>
+                    <tr><td>Lee Gillam, University of Surrey, United Kingdom</td></tr>
+                    <tr><td>Lorenzo Carnevale, University of Messina, Italy</td></tr>
+                    <tr><td>Luca Davoli, University of Parma, Italy</td></tr>
+                    <tr><td>Marc Lacoste, Orange Labs</td></tr>
+                    <tr><td>Peng Zhang, Nanjing University of Science and Technology, China</td></tr>
+                    <tr><td>Radu Prodan, University of Klagenfurt, Austria</td></tr>
+                    <tr><td>Ramin Yahyapour, University of Göttingen, Germany</td></tr>
+                    <tr><td>Thomas Beach, Cardiff University, United Kingdom</td></tr>
+                    <tr><td>Weilong Din, North China University of Technology, China</td></tr>
+                    <tr><td>Xiao Chen, University of Edinburgh, Scotland</td></tr>
+                    </tbody>
+                </table>
+            </Content>
+        </PageWrapper>
+    );
+};
+
+export default SteeringCommittee;
